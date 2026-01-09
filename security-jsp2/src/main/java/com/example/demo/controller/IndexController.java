537d3707338911b6b79f9c997af6f3a2f6bfc7e5
dmstn <eunsoo4629@naver.com>
2026-01-09 17:07:19 +0900

    회원가입구현 및 테스트

--- security-jsp2/src/main/java/com/example/demo/controller/IndexController.java
+++ security-jsp2/src/main/java/com/example/demo/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
+import com.example.demo.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //스포츠센터
@@ -10,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+    private final MemberService memberService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @GetMapping({"", "/"})
     public String index(){
         log.info("index");
@@ -35,4 +43,38 @@ public class IndexController {
         log.info("admin");
         return "admin";
     }//end of home
+    //로그인 화면 요청하기
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        log.info("loginForm");
+        return "auth/loginForm";
+    }//end of loginForm
+    //회원가입 화면 호출하기
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        log.info("joinForm");
+        // auth/joinForm -> 응답페이지 화면 이름이다
+        // yaml -> /WEB-INF/views/ 접두어
+        // 접미어  -> .jsp
+        return "auth/joinForm";
+    }
+    @GetMapping("/login-error")
+    public String loginError(){
+        log.info("login-error");
+        return "loginError";
+    }
+    //회원가입 구현하기
+    @PostMapping("/join")
+    public String join(User user){
+        log.info("join");
+        user.setRole("ROLE_USER");
+        //패스워드 암호화하기
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        //비번 123으로 등록은 됨. 그러나 시큐리티 로그인 할 수 없음
+        //왜냐면 암호화가 되지 않은 비번에 대해서는 처리안됨
+        user.setPassword(encPassword);
+        memberService.memberInsert(user);
+        return "auth/loginForm";
+    }
 }
